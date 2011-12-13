62b2701c06f798d03d943a81596cb17f1ba586de
julien <julien@gamr7.com>
2011-12-13 13:12:02 +0100

    Added method in displayable.

--- displayable.h
+++ displayable.h
@@ -1,5 +1,8 @@
 #ifndef Displayable_H
 #define Displayable_H
+#include "displayer.h"
+
+class Displayer;
 
 class Displayable
 {
@@ -7,7 +10,9 @@ public:
     Displayable();
     virtual int get_x();
     virtual int get_y();
-    
+    void set_displayer(Displayer * displayer);
+protected:
+    Displayer * m_displayer;
 };
 
 #endif
