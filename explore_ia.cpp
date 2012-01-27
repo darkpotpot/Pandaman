d8f57f73e0e02f42c47c26f369412ac788f69446
Daniel <daniel@PC-de-daniel.(none)>
2012-01-27 19:32:43 +0100

    renaming a class that got a terrible name during debugging

--- explore_ia.cpp
+++ explore_ia.cpp
@@ -6,13 +6,13 @@
 
 
 
-PandaManPoint2d operator+(const PandaManPoint2d& p1, const PandaManPoint2d& p2)
+Point2d operator+(const Point2d& p1, const Point2d& p2)
 {
-    return PandaManPoint2d(p1.get_x()+ p2.get_x(), p1.get_y()+p2.get_y()); 
+    return Point2d(p1.get_x()+ p2.get_x(), p1.get_y()+p2.get_y()); 
 }
 
 //necessary because in order to be used as key in map...need to be comparable...
-bool operator<(const PandaManPoint2d& p1, const PandaManPoint2d& p2)
+bool operator<(const Point2d& p1, const Point2d& p2)
 {
     if (p2.get_x()>p1.get_x())
         {return true;}
@@ -23,17 +23,17 @@ bool operator<(const PandaManPoint2d& p1, const PandaManPoint2d& p2)
 
 }
 
-list<PandaManPoint2d> init_directions()
+list<Point2d> init_directions()
 {
-        list<PandaManPoint2d> accessible_directions;
-        accessible_directions.push_back(PandaManPoint2d(-1,0));
-        accessible_directions.push_back(PandaManPoint2d(1,0));
-        accessible_directions.push_back(PandaManPoint2d(0,-1));
-        accessible_directions.push_back(PandaManPoint2d(0,1));
+        list<Point2d> accessible_directions;
+        accessible_directions.push_back(Point2d(-1,0));
+        accessible_directions.push_back(Point2d(1,0));
+        accessible_directions.push_back(Point2d(0,-1));
+        accessible_directions.push_back(Point2d(0,1));
         return accessible_directions;
 }
 
-list<PandaManPoint2d>  ExploreIA::m_accessible_directions(init_directions());
+list<Point2d>  ExploreIA::m_accessible_directions(init_directions());
 
 
 ExploreIA::ExploreIA()
@@ -46,21 +46,21 @@ ExploreIA::~ExploreIA()
 
 void ExploreIA::compute_movement(Entity *entity, Grid* grid, int& x_res, int&y_res)
 {
-    list<PandaManPoint2d> accessible_pos;
-    get_accessible_neightboors(grid, PandaManPoint2d(entity->get_x(), entity->get_y()), accessible_pos);
+    list<Point2d> accessible_pos;
+    get_accessible_neightboors(grid, Point2d(entity->get_x(), entity->get_y()), accessible_pos);
     int min_value =-1;
     int current_value = -1;
-    PandaManPoint2d best_pos;
-    for (list<PandaManPoint2d>::iterator it=accessible_pos.begin(); it!=accessible_pos.end(); it++)
+    Point2d best_pos;
+    for (list<Point2d>::iterator it=accessible_pos.begin(); it!=accessible_pos.end(); it++)
         {
-            std::map<PandaManPoint2d,int>::iterator res = m_exploration_map.find((*it));
+            std::map<Point2d,int>::iterator res = m_exploration_map.find((*it));
             if (res!=m_exploration_map.end())
             {
                 current_value = res->second;
             }
             else
             {
-                m_exploration_map.insert(std::pair<PandaManPoint2d,int>((*it),0));
+                m_exploration_map.insert(std::pair<Point2d,int>((*it),0));
             current_value = 0;
             }
             if (min_value==-1||current_value<min_value)
@@ -84,17 +84,17 @@ void ExploreIA::compute_movement(Entity *entity, Grid* grid, int& x_res, int&y_r
 
 }
 
-list<PandaManPoint2d>& ExploreIA::get_accessible_direction()
+list<Point2d>& ExploreIA::get_accessible_direction()
 {
     return m_accessible_directions;
 }
 
 
-void ExploreIA::get_accessible_neightboors(Grid* grid, PandaManPoint2d& current_pos, list<PandaManPoint2d>& res_list)
+void ExploreIA::get_accessible_neightboors(Grid* grid, Point2d& current_pos, list<Point2d>& res_list)
 {
-    PandaManPoint2d pos;
-    list<PandaManPoint2d>& accessible_directions = get_accessible_direction();
-    for (list<PandaManPoint2d>::iterator it = accessible_directions.begin();
+    Point2d pos;
+    list<Point2d>& accessible_directions = get_accessible_direction();
+    for (list<Point2d>::iterator it = accessible_directions.begin();
         it!=accessible_directions.end();
         it++)
         {
--- explore_ia.h
+++ explore_ia.h
@@ -3,11 +3,11 @@
 #include "ia.h"
 #include <map>
 
-class PandaManPoint2d{
+class Point2d{
 
 public:
-    PandaManPoint2d():m_x(0), m_y(0){}
-    PandaManPoint2d(int x, int y):m_x(x), m_y(y){}
+    Point2d():m_x(0), m_y(0){}
+    Point2d(int x, int y):m_x(x), m_y(y){}
     int get_x() const {return m_x;}
     int get_y()const {return m_y;}
     
@@ -23,15 +23,15 @@ public:
     virtual void compute_movement(Entity *entity, Grid* grid, int& x_res, int&y_res);
  
 private:
-    void get_accessible_neightboors(Grid* grid, PandaManPoint2d& current_pos, list<PandaManPoint2d>& res_list);
-    list<PandaManPoint2d>& get_accessible_direction();
-    static list<PandaManPoint2d> m_accessible_directions;
-    std::map<PandaManPoint2d, int> m_exploration_map;
+    void get_accessible_neightboors(Grid* grid, Point2d& current_pos, list<Point2d>& res_list);
+    list<Point2d>& get_accessible_direction();
+    static list<Point2d> m_accessible_directions;
+    std::map<Point2d, int> m_exploration_map;
 };
 
-PandaManPoint2d operator+(const PandaManPoint2d& p1, const PandaManPoint2d& p2);
+Point2d operator+(const Point2d& p1, const Point2d& p2);
 
 //necessary because in order to be used as key in map...need to be comparable...
-bool operator<(const PandaManPoint2d& p1, const PandaManPoint2d& p2);
+bool operator<(const Point2d& p1, const Point2d& p2);
 
 #endif
